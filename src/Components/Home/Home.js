b9d10994eb0adce862986900e973b8d10f73daf3
Arnob3108 <sajedurrahmanarnob@gmail.com>
2022-10-27 19:53:06 +0600

    typewritter added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
 import AwsomeSlider from "./AwsomeSlider";
 import CourseInfo from "./CourseInfo";
 import Sponsor from "./Sponsor";
@@ -10,19 +11,24 @@ const Home = () => {
       <h1 className="text-center text-xl lg:text-4xl text-white font-extrabold mt-[3%]">
         Welcome <br /> To Our <br />{" "}
       </h1>
-      <p className="text-white text-center text-2xl lg:text-5xl w-[60%] lg:w-[25%] mx-auto bg-blue-500/50 py-5 mt-3 font-bold px-3 rounded-2xl">
-        Innovative Design
+      <p className="text-white text-center uppercase text-xl lg:text-5xl w-[90%] mb-[5%] lg:mb-0 lg:w-[65%] mx-auto bg-blue-500/50 py-5 mt-3 font-bold px-3 rounded-2xl">
+        <Typewriter
+          words={[
+            "Innovative Design",
+            "Get the creativity out of you!",
+            "Be a professional Designer.",
+            "Check out our courses!",
+            "select your desire course! Bhoom!!!",
+          ]}
+          loop={true}
+          cursor
+          cursorStyle="_"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
       </p>
-      <h1 className="text-center text-2xl lg:text-4xl text-blue-500 font-extrabold mt-[2%]">
-        Set Your{" "}
-        <Link
-          className=" transition ease-in-out hover:-translate-y-1 hover:text-blue-700 duration-500 "
-          to="/courses"
-        >
-          Course
-        </Link>{" "}
-        & Achive Your Goal!!
-      </h1>
+
       <AwsomeSlider></AwsomeSlider>
       <CourseInfo></CourseInfo>
       <Sponsor></Sponsor>
